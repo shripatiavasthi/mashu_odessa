f574027da64f49d393bbcde95deaf0e7a47b842f
verb-shripati <73355006+shripatiavasthi@users.noreply.github.com>
2026-01-08 19:26:31 +0530

    fix try 1

--- ios/odessa_mobile_app/AppDelegate.swift
+++ ios/odessa_mobile_app/AppDelegate.swift
@@ -5,6 +5,7 @@ import ReactAppDependencyProvider
 import FirebaseCore
 @objc(RNAppAuthAuthorizationFlowManagerDelegate)
 protocol RNAppAuthAuthorizationFlowManagerDelegate {
+  @objc(resumeExternalUserAgentFlowWithURL:)
   func resumeExternalUserAgentFlow(with url: URL) -> Bool
 }
 
